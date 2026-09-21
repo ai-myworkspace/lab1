8ac12b0c549d1c1e8b02ae24dca9d2af0b31191d
Thach.Le <thachln@gmail.com>
2026-09-22 00:07:17 +0700

    Prepare class MyBigNumber.java with Springboot.

---
+++ src/main/java/myworkspace/aidev/MyBigNumber.java
@@ -0,0 +1,16 @@
+package myworkspace.aidev;
+
+public class MyBigNumber {
+
+	/**
+	 * Computes the sum of two big numbers represented as strings.
+	 * 
+	 * @param stn1
+	 * @param stn2
+	 * @return
+	 */
+	public String sum(String stn1, String stn2) {
+		return null;
+	}
+
+}
